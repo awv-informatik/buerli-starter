a9e51ade464a3ff7de8e5f676859169cc971d500
— <drcmda@gmail.com>
2023-05-04 11:06:04 +0200

    first draft

---
+++ packages/with-headless-history-run/src/components/Scene.jsx
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+import { useLayoutEffect, useRef } from 'react'
+import { Center, Resize, Bounds } from '@react-three/drei'
+import { BuerliGeometry } from '@buerli.io/react'
+
+export function Scene(props) {
+  const geometry = useRef()
+
+  useLayoutEffect(() => {
+    if (geometry.current) {
+      geometry.current.traverse((obj) => {
+        obj.receiveShadow = obj.castShadow = true
+        if (obj.type === 'Mesh') {
+          obj.material = new THREE.MeshStandardMaterial({ color: 'orange', roughness: 0.5 })
+        }
+      })
+    }
+  })
+
+  return (
+    <group {...props}>
+      <Bounds fit clip observe>
+        <Resize scale={1}>
+          <Center top ref={geometry}>
+            <BuerliGeometry suspend />
+          </Center>
+        </Resize>
+      </Bounds>
+    </group>
+  )
+}
---
+++ packages/with-headless-history-run/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import App from './components/App'
+
+createRoot(document.getElementById('root')).render(<App />)
