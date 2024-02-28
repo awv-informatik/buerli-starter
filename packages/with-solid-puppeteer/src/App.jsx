ad367dba40c3e09d7169b6fc0044cc6507d8ec39
— <drcmda@gmail.com>
2024-02-28 10:18:25 +0100

    cli changes

--- packages/with-solid-puppeteer/src/App.jsx
+++ packages/with-solid-puppeteer/src/App.jsx
@@ -9,15 +9,24 @@ import tunnel from 'tunnel-rat'
 const t = tunnel()
 const { cache, useDrawingId } = headless(Solid, 'ws://localhost:9091')
 
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const file = urlParams.get('file')
+const shadows = JSON.parse(urlParams.get('shadows'))
+const bg = urlParams.get('bg') ?? '#f0f0f0'
+const ambience = urlParams.get('ambience') ?? 1
+const diffuse = urlParams.get('diffuse') ?? Math.PI
+const direction = JSON.parse(urlParams.get('direction')) ?? [10, 10, 10]
+
 export default function App() {
   return (
     <>
-      <Canvas gl={{ preserveDrawingBuffer: true }} shadows orthographic camera={{ position: [10, 10, 10], zoom: 450 }}>
-        <color attach="background" args={['#f0f0f0']} />
-        <ambientLight />
+      <Canvas gl={{ preserveDrawingBuffer: true }} shadows={shadows} orthographic camera={{ position: direction, zoom: 450 }}>
+        <color attach="background" args={[bg]} />
+        <ambientLight intensity={ambience} />
         <spotLight
           decay={0}
-          intensity={Math.PI}
+          intensity={diffuse}
           position={[10, 15, -15]}
           angle={0.4}
           penumbra={1}
@@ -46,18 +55,10 @@ export default function App() {
 function Model(props) {
   const ref = useRef()
   const drawingId = useDrawingId()
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
-  const file = urlParams.get('file')
 
   useLayoutEffect(() => {
     ref.current.traverse(child => {
-      if (child.isMesh) {
-        child.castShadow = child.receiveShadow = true        
-        //child.material = new THREE.MeshStandardMaterial()
-        //child.material.roughness = 0
-        //child.material.metalness = 1
-      }
+      if (child.isMesh) child.castShadow = child.receiveShadow = true
     })
   }, [])
 
